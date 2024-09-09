9e982ba6c81448f7c8d2ca24191de0b7b9b57764
Dawei Hao <dwsamurai84@gmail.com>
2024-09-09 14:38:00 +0800

    Initial Commit

---
+++ BAT_iOS_testing/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BAT_iOS_testing
+//
+//  Created by Dawei Hao on 2024/9/9.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
